d5efb3b836a8a2b6716058245e6cec3e679c7485
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-12 23:29:08 +0900

    feat: 사용자의 주문 입력에 대한 유효성 검사 기능 추가

---
+++ src/main/java/christmas/View/InputValidator/OrderValidator.java
@@ -0,0 +1,34 @@
+package christmas.View.InputValidator;
+
+import static christmas.Constants.Message.ExceptionMsg.OrderErrorMsg.ASK_NO_EMPTY_INPUT;
+import static christmas.Constants.Message.ExceptionMsg.OrderErrorMsg.ASK_NO_WHITE_SPACE;
+import static christmas.Constants.Message.ExceptionMsg.OrderErrorMsg.ASK_ORDER_INPUT_IN_VALID_FORMAT;
+
+public class OrderValidator {
+    public final String WHITE_SPACE = " ";
+    private final String VALID_FORMAT_REGULAR_EXPRESSION = "^(?:[가-힣]+-\\d+,)*(?:[가-힣]+-\\d+)$";
+
+    public void validate(String orderUserInput){
+        isNotEmpty(orderUserInput);
+        containsNoWhiteSpace(orderUserInput);
+        isRightFormat(orderUserInput);
+    }
+
+    public void isNotEmpty(String orderUserInput) throws IllegalArgumentException{
+        if(orderUserInput.isEmpty()){
+            throw new IllegalArgumentException(ASK_NO_EMPTY_INPUT.getMessage());
+        }
+    }
+
+    public void containsNoWhiteSpace(String orderUserInput) throws IllegalArgumentException{
+        if(orderUserInput.contains(WHITE_SPACE)){
+            throw new IllegalArgumentException(ASK_NO_WHITE_SPACE.getMessage());
+        }
+    }
+
+    public void isRightFormat(String orderUserInput) throws IllegalArgumentException{
+        if(!orderUserInput.matches(VALID_FORMAT_REGULAR_EXPRESSION)){
+            throw new IllegalArgumentException(ASK_ORDER_INPUT_IN_VALID_FORMAT.getMessage());
+        }
+    }
+}
