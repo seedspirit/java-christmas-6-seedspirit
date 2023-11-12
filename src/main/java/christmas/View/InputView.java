ce24401e5cb61423248d530040e06ed30a8e68dd
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-12 23:29:43 +0900

    feat: 사용자의 입력을 받아 유효성 검사, 변환까지 하는 view 코드 추가

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -1,32 +1,49 @@
 package christmas.View;
 
+import static christmas.Constants.Message.ExceptionMsg.OrderErrorMsg.ASK_ORDER_INPUT_IN_VALID_FORMAT;
 import static christmas.Constants.Message.InputPromptMsg.ASK_FOOD_ORDER;
 import static christmas.Constants.Message.InputPromptMsg.ASK_VISITING_DATE;
 import static christmas.Constants.Message.InputPromptMsg.DEFAULT_WELCOME_COMMENT;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.View.InputConvertor.InputConvertor;
 import christmas.View.InputValidator.VisitDateValidator;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class InputView {
     VisitDateValidator visitDateValidator = new VisitDateValidator();
-    public void printDefaultWelcomeMsg(){
+    InputConvertor convertor = new InputConvertor();
+
+    InputView(){
         System.out.println(DEFAULT_WELCOME_COMMENT.getMessage());
     }
+
     public Integer visitDateInput(){
         while (true) {
             try {
                 System.out.println(ASK_VISITING_DATE.getMessage());
                 String visitDateUserInput = Console.readLine();
                 visitDateValidator.validate(visitDateUserInput);
-                return Integer.parseInt(visitDateUserInput);
+                return convertor.convertVisitDate(visitDateUserInput);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    public String foodOrderInput(){
-        System.out.println(ASK_FOOD_ORDER.getMessage());
-        return Console.readLine();
+    public Map<String, Integer> orderInput(){
+        while (true) {
+            try {
+                System.out.println(ASK_FOOD_ORDER.getMessage());
+                String orderUserInput = Console.readLine();
+                visitDateValidator.validate(orderUserInput);
+                return convertor.covertOrderInput(orderUserInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
