a07aab605168d3d0c5b8edc3fbcd069eaac77986
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-13 21:30:09 +0900

    feat: 애플리케이션 도메인에서 사용하는 상수 추가

---
+++ src/main/java/christmas/Constants/DomainConstants.java
@@ -0,0 +1,12 @@
+package christmas.Constants;
+
+public class DomainConstants {
+    public static final Integer YEAR_2023 = 2023;
+    public static final Integer EVENT_START_DATE = 1;
+    public static final Integer EVENT_END_DATE = 31;
+    public static final Integer CHRISTMAS_DAY = 25;
+    public static final Integer ZERO = 0;
+    public static final Integer MIN_FOOD_AMOUNT = 1;
+    public static final Integer MAX_FOOD_AMOUNT = 20;
+    public static final Integer MIN_PARTICIPATION_FEE = 10_000;
+}
