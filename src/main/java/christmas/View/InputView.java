830d44f3d942b9029f8720c6e0a055fd0db9cfd8
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-14 23:32:43 +0900

    fix: 음식 주문에 방문날짜 유효성 검사 도구가 아닌 음식 주문 유효성 검사 실행

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -1,23 +1,22 @@
 package christmas.View;
 
-import static christmas.Constants.Message.ExceptionMsg.OrderErrorMsg.ASK_ORDER_INPUT_IN_VALID_FORMAT;
 import static christmas.Constants.Message.InputPromptMsg.ASK_FOOD_ORDER;
 import static christmas.Constants.Message.InputPromptMsg.ASK_VISITING_DATE;
 import static christmas.Constants.Message.InputPromptMsg.DEFAULT_WELCOME_COMMENT;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.Model.FoodOrderDetails;
 import christmas.View.InputConvertor.InputConvertor;
+import christmas.View.InputValidator.OrderValidator;
 import christmas.View.InputValidator.VisitDateValidator;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class InputView {
     VisitDateValidator visitDateValidator = new VisitDateValidator();
+    OrderValidator orderValidator = new OrderValidator();
     InputConvertor convertor = new InputConvertor();
 
-    InputView(){
+    public InputView(){
         System.out.println(DEFAULT_WELCOME_COMMENT.getMessage());
     }
 
@@ -39,7 +38,7 @@ public class InputView {
             try {
                 System.out.println(ASK_FOOD_ORDER.getMessage());
                 String orderUserInput = Console.readLine();
-                visitDateValidator.validate(orderUserInput);
+                orderValidator.validate(orderUserInput);
                 return convertor.covertOrderInput(orderUserInput);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
