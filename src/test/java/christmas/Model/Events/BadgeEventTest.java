8f818394c196c97ec094bafa018999533d50333d
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-15 16:15:13 +0900

    fix: 가독성을 위해 코드 폴더 구조 변경

--- src/test/java/christmas/Model/Events/BadgeEventTest.java
+++ src/test/java/christmas/Model/Events/BadgeEventTest.java
@@ -2,7 +2,7 @@ package christmas;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import christmas.Model.BadgeEvent;
+import christmas.Model.Events.BadgeEvent;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
