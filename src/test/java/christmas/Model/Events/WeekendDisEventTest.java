0400b79518209ad34ad3d042503f2cd8a324471d
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-15 16:49:59 +0900

    test: 주말 할인 이벤트 테스트 코드 추가

--- src/test/java/christmas/Model/Events/WeekendDisEventTest.java
+++ src/test/java/christmas/Model/Events/WeekendDisEventTest.java
@@ -2,8 +2,9 @@ package christmas.Model.Events;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.Model.Benefits;
 import christmas.Model.FoodOrderDetails;
-import christmas.Utils.DayOfWeekConvertor;
+import christmas.Model.ReservationDetails;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -13,21 +14,24 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class WeekendDisEventTest {
 
-    private static Stream<Arguments> provideUserInputInWeekend(){
+    private static Stream<Arguments> provideUserInput(){
         return Stream.of(
-                Arguments.of(new FoodOrderDetails(Map.of("바비큐립", 1)), 1, 2023),
-                Arguments.of(new FoodOrderDetails(Map.of("티본스테이크", 3, "제로콜라", 1, "초코케이크", 2)), 8, 6069),
-                Arguments.of(new FoodOrderDetails(Map.of("티본스테이크", 3, "제로콜라", 1, "초코케이크", 2)), 12, 0),
+                Arguments.of(new FoodOrderDetails(Map.of("바비큐립", 1)), 1, 0),
+                Arguments.of(new FoodOrderDetails(Map.of("티본스테이크", 3, "제로콜라", 1, "초코케이크", 2)), 25, 25000),
+                Arguments.of(new FoodOrderDetails(Map.of("티본스테이크", 3, "제로콜라", 1, "초코케이크", 2)), 12, 25000),
                 Arguments.of(new FoodOrderDetails(Map.of("제로콜라", 1, "초코케이크", 2)), 12, 0)
                 );
     }
 
-    @DisplayName("주말 할인 이벤트 금액 계산 테스트")
+    @DisplayName("증정 이벤트 테스트")
     @ParameterizedTest
-    @MethodSource("provideUserInputInWeekend")
-    void 주말_할인_이벤트_금액_계산_테스트(FoodOrderDetails order, Integer visitDate, Integer expectedDisAmount){
-        WeekendDisEvent event = new WeekendDisEvent();
-        assertThat(event.applyEvent(order, DayOfWeekConvertor.convert(visitDate)))
+    @MethodSource("provideUserInput")
+    void 증정_이벤트_테스트(FoodOrderDetails order, Integer visitDate, Integer expectedDisAmount){
+        ReservationDetails reservation = new ReservationDetails(order, visitDate);
+        Benefits benefits = new Benefits();
+        benefits.apply(reservation);
+        GiveAwayEvent event = new GiveAwayEvent();
+        assertThat(event.applyEvent(reservation.getFoodOrderDetails().getPreDiscountTotal()))
                 .isEqualTo(expectedDisAmount);
     }
 }
