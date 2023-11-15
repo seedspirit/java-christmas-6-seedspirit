17fe3f46e0256b08782b307cb9f055251a897e40
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-15 20:20:23 +0900

    refactor: 공통적인 부분을 하나의 메서드로 추출

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -5,41 +5,48 @@ import static christmas.Constants.Message.InputPromptMsg.ASK_VISITING_DATE;
 import static christmas.Constants.Message.InputPromptMsg.DEFAULT_WELCOME_COMMENT;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.Model.FoodOrderDetails;
-import christmas.View.InputConvertor.InputConvertor;
+import christmas.View.InputConverter.InputConverter;
 import christmas.View.InputValidator.OrderValidator;
+import christmas.View.InputValidator.Validator;
 import christmas.View.InputValidator.VisitDateValidator;
+
 import java.util.Map;
+import java.util.function.Function;
 
 public class InputView {
-    VisitDateValidator visitDateValidator = new VisitDateValidator();
-    OrderValidator orderValidator = new OrderValidator();
-    InputConvertor convertor = new InputConvertor();
+
+    InputConverter converter = new InputConverter();
+
+    Validator visitDateValidator = new VisitDateValidator();
+    Validator orderValidator = new OrderValidator();
 
     public InputView(){
         System.out.println(DEFAULT_WELCOME_COMMENT.getMessage());
     }
 
     public Integer visitDateInput(){
-        while (true) {
-            try {
-                System.out.println(ASK_VISITING_DATE.getMessage());
-                String visitDateUserInput = Console.readLine();
-                visitDateValidator.validate(visitDateUserInput);
-                return convertor.convertVisitDate(visitDateUserInput);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        return inputWithValidation(
+                ASK_VISITING_DATE.getMessage(),
+                visitDateValidator,
+                converter::convertVisitDate
+        );
     }
 
     public Map<String, Integer> orderInput(){
+        return inputWithValidation(
+                ASK_FOOD_ORDER.getMessage(),
+                orderValidator,
+                converter::covertOrderInput
+        );
+    }
+
+    private <T> T inputWithValidation(String message, Validator validator, Function<String, T> converter){
         while (true) {
             try {
-                System.out.println(ASK_FOOD_ORDER.getMessage());
-                String orderUserInput = Console.readLine();
-                orderValidator.validate(orderUserInput);
-                return convertor.covertOrderInput(orderUserInput);
+                System.out.println(message);
+                String userInput = Console.readLine();
+                validator.validate(userInput);
+                return converter.apply(userInput);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
